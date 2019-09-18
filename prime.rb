49498ecc96d7aa81705e6c35d0b8c193069e7c7d
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-18 00:33:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,8 @@ def prime?(number)
   
   if number > 2
    numberArray = (2..number-1)
-   numberArray.any?() { |num| number % num == 0 }
+   numberArray.none?() { |num| number % num == 0 }
+   
   else
     false
   end
