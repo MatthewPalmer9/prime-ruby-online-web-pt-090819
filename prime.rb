b29b5e313e5277f04e04c41c6aa4070db1d83aab
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-18 00:15:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,8 @@ def prime?(number)
   if number == 2 
     return true 
   end 
-
-  if number < 2 || !(number % 2)
-    return false 
-  end
   
-  i = 3
-  while i <= (number - 1)
-    i += 1
-    if number % i == 0 
-      return false
-    end
-  end
-  return true
+  if number > 2 
+    range = rand(2..(number-1)).to_a 
+    
 end
